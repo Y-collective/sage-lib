4d8f7815df5ac9795344bde0b505f877354b9061
Milan Bartalovics <bartalovics.milan@yco.hu>
2023-10-10 14:55:10 +0200

    add missing functions to Container.php

--- Container.php
+++ Container.php
@@ -6,4 +6,39 @@ use Illuminate\Container\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
+    /**
+     * The array of terminating callbacks.
+     *
+     * @var callable[]
+     */
+    protected $terminatingCallbacks = [];
+
+    /**
+     * Register a terminating callback with the application.
+     *
+     * @param callable|string $callback
+     * @return $this
+     */
+    public function terminating($callback)
+    {
+        $this->terminatingCallbacks[] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Terminate the application.
+     *
+     * @return void
+     */
+    public function terminate()
+    {
+        $index = 0;
+
+        while ($index < count($this->terminatingCallbacks)) {
+            $this->call($this->terminatingCallbacks[$index]);
+
+            $index++;
+        }
+    }
 }
